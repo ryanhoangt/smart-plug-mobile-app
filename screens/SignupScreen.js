6f87a09163f6e12e8429f73e20f2f734d0e01a71
Minh Nguyễn <87883380+tonminhce@users.noreply.github.com>
2023-03-14 17:41:05 +0700

    update lần 1 các screen onboard, login, signup
    chưa add thêm font cho onboard screen

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -1,22 +1,149 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+} from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
-function SignupScreen() {
+const SignupScreen = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const navigation = useNavigation();
+  const handleShowPassword = () => {
+    setShowPassword(!showPassword);
+  };
+      const getLoginHandler = () => {
+        navigation.replace("Login");
+      };
+
   return (
     <View style={styles.container}>
-      <StatusBar style="auto" />
-      <Text>Signup Screen</Text>
+      <Text style={styles.title}>Sign Up</Text>
+      <Text style={styles.label}>Name</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your name"
+        value={name}
+        onChangeText={setName}
+      />
+      <Text style={styles.label}>Email</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <Text style={styles.label}>Password</Text>
+      <View style={styles.passwordContainer}>
+        <TextInput
+          style={[styles.passwordInput]}
+          placeholder="Enter your password"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry={!showPassword}
+        />
+        <TouchableOpacity
+          onPress={handleShowPassword}
+          style={styles.passwordIconContainer}
+        >
+          <MaterialCommunityIcons
+            name={showPassword ? "eye-off" : "eye"}
+            size={24}
+            color="grey"
+          />
+        </TouchableOpacity>
+      </View>
+      <TouchableOpacity style={styles.signupButton}>
+        <Text style={styles.signupButtonText}>Sign Up</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={getLoginHandler}>
+        <Text style={styles.alreadyButtonText}>Already have an account ?</Text>
+      </TouchableOpacity>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    justifyContent: "top",
     alignItems: "center",
+    paddingHorizontal: 20,
+    backgroundColor: "#fff",
+  },
+  title: {
+    marginTop: 80,
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 30,
+  },
+  label: {
+    fontSize: 18,
+    fontWeight: "bold",
+    alignSelf: "flex-start",
+    marginBottom: 5,
+  },
+  input: {
+    height: 50,
+    width: "100%",
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+  },
+  nameContainer: {
+    height: 50,
+    width: "100%",
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  passwordContainer: {
+    height: 50,
+    width: "100%",
+    borderWidth: 1,
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    marginBottom: 40,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  passwordInput: {
+    flex: 1,
+  },
+  passwordIconContainer: {
+    height: "100%",
+    aspectRatio: 1,
     justifyContent: "center",
+    alignItems: "center",
+  },
+  alreadyButtonText: {
+    fontSize: 16,
+    color: "rgba(250,100,0,1)",
+  },
+  signupButton: {
+    height: 50,
+    width: "100%",
+    borderWidth: 0,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 20,
+    backgroundColor: "#7EAFD5",
+  },
+  signupButtonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "white",
   },
 });
-
 export default SignupScreen;
