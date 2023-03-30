bc98567a95cda6465e4fa29b237733f069cae3e0
datdev2409 <datdev2409@gmail.com>
2023-03-30 14:48:16 +0700

    edit paddding for onboard screen

--- screens/OnboardScreen.js
+++ screens/OnboardScreen.js
@@ -79,7 +79,7 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    // paddingTop: 80,
+    paddingTop: 50,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'flex',
